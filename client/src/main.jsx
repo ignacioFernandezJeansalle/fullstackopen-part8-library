c4ee96f00ec62cb4e94517499fc2aba143f29e81
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-20 16:18:28 -0300

    Create client of Apollo and add ApolloProvider component (ex8.8)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from "@apollo/client";
+
 import App from "./App.jsx";
 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({ uri: "http://localhost:4000" }),
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
