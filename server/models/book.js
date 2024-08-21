ebf967fea7069cbff00e2b133a12c2ec1384932d
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-21 13:26:02 -0300

    Create schema and model mongoose for Book

---
+++ server/models/book.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: true,
+    unique: true,
+    minlength: 2,
+  },
+  published: {
+    type: Number,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Author",
+  },
+  genres: [{ type: String }],
+});
+
+const Book = mongoose.model("Book", bookSchema);
+export default Book;
