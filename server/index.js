d4eb84392014f0ff9264c10a68b9aa2df27dea96
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-22 18:21:22 -0300

    Add currentUser in context to startStandaloneServer

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import jwt from "jsonwebtoken";
+const { verify: jwtVerify } = jwt;
 import { typeDefs } from "./graphql/schema.js";
 import { resolvers } from "./graphql/resolvers.js";
+import User from "./models/user.js";
 
 import "dotenv/config";
 import mongoose from "mongoose";
@@ -25,6 +28,14 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null;
+    if (auth && auth.startsWith("Bearer ")) {
+      const decodedToken = jwtVerify(auth.substring(7), process.env.JWT_SECRET);
+      const currentUser = await User.findById(decodedToken.id);
+      return { currentUser };
+    }
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
