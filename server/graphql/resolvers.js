c1456a73432d02f19ee0dab2bf6fdc088d4f339f
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-22 13:02:57 -0300

    Fix all queries and mutation (ex8.14)

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -3,26 +3,17 @@ import Author from "../models/author.js";
 
 export const resolvers = {
   Query: {
-    bookCount: async () => await Book.countDocuments(),
-    authorCount: async () => await Author.countDocuments(),
-    allBooks: async () => {
-      const allBooks = await Book.find({}).populate("author");
-
-      return allBooks;
-
-      /* if (!args.author && !args.genre) return books;
-
-      let filteredBooks = [...books];
-      if (args.author) filteredBooks = filteredBooks.filter((book) => book.author === args.author);
-      if (args.genre) filteredBooks = filteredBooks.filter((book) => book.genres.includes(args.genre));
-      return filteredBooks; */
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
+      const query = args.genre ? { genres: { $elemMatch: { $eq: args.genre } } } : {};
+      return Book.find(query).populate("author");
     },
-    allAuthors: async () => {
-      const allAuthors = await Author.find({});
+    allAuthors: async () => Author.find({}),
+  },
 
-      /* bookCount: books.reduce((count, book) => (author.name === book.author ? count + 1 : count), 0) */
-      return allAuthors;
-    },
+  Author: {
+    bookCount: (root) => root.books.length,
   },
 
   Mutation: {
@@ -30,25 +21,25 @@ export const resolvers = {
       let author = await Author.findOne({ name: args.author });
 
       if (!author) {
-        const newAuthor = new Author({ name: args.author, born: 0 });
+        const newAuthor = new Author({ name: args.author, born: 0, books: [] });
         author = await newAuthor.save();
       }
 
       const newBook = new Book({ ...args, author: author._id });
       const newBookSave = await newBook.save();
 
-      const response = await Book.findById(newBookSave._id).populate("author");
+      author.books.push(newBookSave._id);
+      await author.save();
 
-      return response;
+      return Book.findById(newBookSave._id).populate("author");
     },
-    /* editAuthor: (root, args) => {
-      const foundAuthorIndex = authors.findIndex((author) => author.name === args.name);
+    editAuthor: async (root, args) => {
+      let author = await Author.findOne({ name: args.name });
 
-      if (foundAuthorIndex === -1) return null;
+      if (!author) return null;
 
-      const editedAuthor = { ...authors[foundAuthorIndex], born: args.setBornTo };
-      authors[foundAuthorIndex] = editedAuthor;
-      return editedAuthor;
-    }, */
+      author.born = args.setBornTo;
+      return author.save();
+    },
   },
 };
