a41c102c5b960198ea4102851a86b33be271dd46
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-23 11:35:43 -0300

    Add user login/logout and refactor conditional renders (ex8.18)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useApolloClient } from "@apollo/client";
+
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
+  const [token, setToken] = useState(null);
   const [page, setPage] = useState("authors");
+  const client = useApolloClient();
+
+  useEffect(() => {
+    const token = localStorage.getItem("library-user-token");
+    if (token) setToken(token);
+  }, []);
+
+  useEffect(() => {
+    setPage("authors");
+  }, [token]);
+
+  const logout = () => {
+    setToken(null);
+    localStorage.removeItem("library-user-token");
+    client.resetStore();
+  };
 
   return (
     <div>
       <div>
         <button onClick={() => setPage("authors")}>authors</button>
         <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
+        {!token && <button onClick={() => setPage("login")}>login</button>}
+        {token && (
+          <>
+            <button onClick={() => setPage("add")}>add book</button>
+            <button onClick={logout}>logout</button>
+          </>
+        )}
       </div>
 
-      {page === "authors" && <Authors />}
+      {page === "authors" && <Authors token={token} />}
       {page === "books" && <Books />}
       {page === "add" && <NewBook />}
+      {page === "login" && <LoginForm setToken={setToken} />}
     </div>
   );
 };
