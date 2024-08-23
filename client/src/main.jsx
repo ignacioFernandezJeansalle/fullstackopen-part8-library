3c8b2655f9ca506a433dad3f727abec145c8144a
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-23 11:34:10 -0300

    Add context for headers with token to ApolloClient

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,27 @@
 import ReactDOM from "react-dom/client";
-import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-import App from "./App.jsx";
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("library-user-token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000",
+});
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({ uri: "http://localhost:4000" }),
+  link: authLink.concat(httpLink),
 });
+
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ApolloProvider client={client}>
