e973fdfe379f3ae833117cb57c465a4cfe9acb38
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-09-13 16:25:52 -0300

    Swap startStandaloneServer to expressMiddleware function

--- server/index.js
+++ server/index.js
@@ -1,17 +1,21 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import jwt from "jsonwebtoken";
-const { verify: jwtVerify } = jwt;
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
 import { typeDefs } from "./graphql/schema.js";
 import { resolvers } from "./graphql/resolvers.js";
+
+import jwt from "jsonwebtoken";
+const { verify: jwtVerify } = jwt;
+
 import User from "./models/user.js";
 
 import "dotenv/config";
 import mongoose from "mongoose";
 mongoose.set("strictQuery", false);
-
 console.log("Connecting to MongoDB");
-
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
@@ -21,21 +25,38 @@ mongoose
     console.log("Error connection to MongoDB: ", error.message);
   });
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+const start = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
 
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-  context: async ({ req }) => {
-    const auth = req ? req.headers.authorization : null;
-    if (auth && auth.startsWith("Bearer ")) {
-      const decodedToken = jwtVerify(auth.substring(7), process.env.JWT_SECRET);
-      const currentUser = await User.findById(decodedToken.id);
-      return { currentUser };
-    }
-  },
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+  await server.start();
+
+  app.use(
+    "/",
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null;
+        if (auth && auth.startsWith("Bearer ")) {
+          const decodedToken = jwtVerify(auth.substring(7), process.env.JWT_SECRET);
+          const currentUser = await User.findById(decodedToken.id);
+          return { currentUser };
+        }
+      },
+    })
+  );
+
+  const PORT = 4000;
+
+  httpServer.listen(PORT, () => {
+    console.log(`Server ready at ${PORT} ***`);
+  });
+};
+
+start();
